3a8d0d1fec585bdeb3dc4fa5d483ea0a4ce63d8e
Thomas Metaxas <pesadathomas@gmail.com>
2021-01-09 02:25:00 -0500

    Update

--- app/src/main/java/com/thomasmetaxas/meteo/MainActivity.java
+++ app/src/main/java/com/thomasmetaxas/meteo/MainActivity.java
@@ -11,6 +11,8 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -18,7 +20,7 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView dateText, cityText, temperatureText, descriptionText;
+    TextView dateText, cityText, temperatureText, descriptionText, feelsLikeText, lowText, highText, humidityText, windText;
     ImageView image;
     String city = "Toronto";
 
@@ -31,6 +33,13 @@ public class MainActivity extends AppCompatActivity {
         cityText = findViewById(R.id.cityText);
         temperatureText = findViewById(R.id.temperatureText);
         descriptionText = findViewById(R.id.descriptionText);
+        feelsLikeText = findViewById(R.id.feelsLikeText);
+        lowText = findViewById(R.id.lowText);
+        highText = findViewById(R.id.highText);
+        humidityText = findViewById(R.id.humidityText);
+        windText = findViewById(R.id.windText);
+
+        afficher();
 
     }
 
@@ -44,6 +53,20 @@ public class MainActivity extends AppCompatActivity {
                     JSONObject main_object = response.getJSONObject("main");
                     JSONArray array = response.getJSONArray("weather");
                     Log.d("Tag", "resultat =" + array.toString());
+                    String temp = response.getJSONObject("main").getString("temp");
+                    String low = response.getJSONObject("main").getString("temp_min");
+                    String high = response.getJSONObject("main").getString("temp_max");
+                    String feelsLike = response.getJSONObject("main").getString("feels_like");
+                    String humidity = response.getJSONObject("main").getString("humidity");
+                    String wind = response.getJSONObject("wind").getString("speed");
+                    String city = response.getString("name");
+                    temperatureText.setText(temp + "°C");
+                    feelsLikeText.setText("Feels like: " + feelsLike + "°C");
+                    lowText.setText("Low: " + low + "°C");
+                    highText.setText("High: " + high + "°C");
+                    humidityText.setText("Humidity: " + humidity + "%");
+                    windText.setText("Wind: " + wind + "km/h");
+                    cityText.setText(city);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -54,6 +77,9 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonObjectRequest);
 
     }
 
